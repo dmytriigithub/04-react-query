a5dad0f1e0b61b774a53b9d03a8f1d096d817383
Dmytrii Datskevych <datskevychdv@gmail.com>
2025-08-31 13:46:48 +0200

    feat: implement movie search with TMDB API and modal window for selected movie

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,66 @@
 import css from "./App.module.css";
+import toast, { Toaster } from "react-hot-toast";
+
+import type { Movie } from "../../types/movie";
+
+import { fetchMovies } from "../../services/movieService";
+
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import Modal from "../MovieModal/MovieModal";
+
+import { useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const openModal = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedMovie(null);
+    setIsModalOpen(false);
+  };
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]);
+      setIsLoading(true);
+      setIsError(false);
+      const data = await fetchMovies(query);
+
+      if (!data.length) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+      setMovies(data);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={css.app}>
-      <p>MoviesApp</p>
+      <Toaster />
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onMovieClick={openModal} />
+      )}
+      {isModalOpen && selectedMovie && (
+        <Modal onClose={closeModal} movie={selectedMovie} />
+      )}
     </div>
   );
 }
